fecaa52ff987e9b9c3a920dbbb08e8c1c2972172
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-02-19 14:51:36 +0530

    third commit and added the CDN links and uses with html and react element

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-const root = document.getElementById('root');
-const header = document.createElement('h1');
-header.innerHTML = 'Hello React using Javacript with theb help of DOM';
-root.appendChild(header);
+//Using CDN Links we create the html element and display it 
+const header = React.createElement('h1', {}, 'Hello React using CDN Links');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(header)
+
