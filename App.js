1c65f8f10484b1f45a8dfa6aca1b91a499cb6922
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-02-19 15:10:57 +0530

    fourth commit and added the Hello React part using parcel and one thing to remember is that if we do npm react then it will run by using parcel only not on live server

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-//Using CDN Links we create the html element and display it 
-const header = React.createElement('h1', {}, 'Hello React using CDN Links');
+import ReactDOM from 'react-dom';
+import React from 'react';
+const header = React.createElement('h1', {}, 'Hello React using parcel');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(header)
+
+
 
