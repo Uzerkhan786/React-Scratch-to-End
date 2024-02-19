1f1942d4597bd47f5ef47664f1270067c2879d6c
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-02-19 14:41:38 +0530

    added HTML using DOM append child

---
+++ App.js
@@ -0,0 +1,4 @@
+const root = document.getElementById('root');
+const header = document.createElement('h1');
+header.innerHTML = 'Hello React using Javacript with theb help of DOM';
+root.appendChild(header);
